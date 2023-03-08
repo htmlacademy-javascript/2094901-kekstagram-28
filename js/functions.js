c3ab9c78380bfe9e155a11a8256daf38665b8599
Vladimir Sapozhnikov <Sapozhnikovl@yandex.ru>
2023-03-08 19:20:14 +0800

    Изменения по линтеру

--- js/functions.js
+++ js/functions.js
@@ -7,8 +7,7 @@ isCheckLength ('Привет', 10);
 // Проверка является ли строка палиндромом.
 
 function getCheckPalindrom(strCheck) {
-  strCheck = strCheck.toUpperCase(),
-  strCheck = strCheck.replaceAll(' ', '');
+  strCheck = strCheck.toUpperCase().replaceAll(' ', '');
   const x = strCheck.length - 1;
   for(let i = 0; i < x / 2; i++) {
     const frwCheck = strCheck[i];
@@ -21,13 +20,6 @@ function getCheckPalindrom(strCheck) {
 }
 
 getCheckPalindrom('Лёша на полке клопа нашёл');
-console.log(getCheckPalindrom('шалаш4fffbf'));
-
-function getCheckPalindrom1(str) {
-  return str === str.toLowerCase().replaceAll(' ', '').split('').reverse().join('');
-}
-
-console.log('шалаш', getCheckPalindrom1('шалашаавм'));
 
 // Функция возвращающая целое положительное число.
 const getNumber = (string) => {
@@ -57,5 +49,4 @@ const getValue = (string, minLength, value) => {
   return result;
 };
 
-getValue('qwe', 10, '01!');
-console.log(getValue('qwe', 10, '01!'));
+getValue('442', 103, '0331!');
